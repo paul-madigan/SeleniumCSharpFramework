a92c933d82961c1366bafb8e5a238b0ebbee1dc4
Paul Madigan <pmadigan25@gmail.com>
2020-04-29 12:30:23 -0500

    Added NUnit Class

--- MyAutomationFramework/Program.cs
+++ MyAutomationFramework/Program.cs
@@ -15,6 +15,10 @@ namespace MyAutomationFramework
         {
             IWebDriver _driver = new ChromeDriver(); //initialize the driver
             _driver.Navigate().GoToUrl("https://www.google.com/"); //open the browser to the specified URL
+
+            // Find an element
+            IWebElement searchBar = _driver.FindElement(By.XPath("//input[@id='fakebox-input']"));
+
             // _driver.Quit(); close all browser windows
             _driver.Close();// close the current browser window
         }
