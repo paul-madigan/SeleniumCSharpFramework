ab8ee32532c312aa4156f149983d7c4df84637ab
Madigan <paul.madigan@accenturefederal.com>
2020-04-29 11:07:00 -0500

    Add project files.

---
+++ MyAutomationFramework/Program.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAutomationFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver _driver = new ChromeDriver(); //initialize the driver
+            _driver.Navigate().GoToUrl("https://www.google.com/"); //open the browser to the specified URL
+            // _driver.Quit(); close all browser windows
+            _driver.Close();// close the current browser window
+        }
+    }
+}
